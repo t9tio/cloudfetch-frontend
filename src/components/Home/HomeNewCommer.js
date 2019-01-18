2640ae840eedc84882f02916a7b0a56a9eb57b6e
timqian <timqian92@qq.com>
2019-01-19 00:39:05 +0800

    add scroll down btn

--- src/components/Home/HomeNewCommer.js
+++ src/components/Home/HomeNewCommer.js
@@ -46,6 +46,15 @@ class Home extends Component {
                     </h2>
                 </div>
             </div>
+            <div className="hero-foot">
+                <div className="container">
+                    <div className="content has-text-centered">
+                        <svg width="35" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path fill="#ffffff" d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </section>
 
         return (
