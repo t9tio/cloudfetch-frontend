c6937a099ae28d73e660bb42c667ebaf6791bbc7
timqian <timqian92@qq.com>
2019-01-28 11:06:39 +0800

    deploy to gh-pages

--- src/components/IsLoading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function IsLoading ({isLoading}) {
-    return (
-        <a className={`button is-white ${isLoading ? 'is-loading' : ''}`} style={{position:'fixed', bottom:0, left:0,background:'transparent'}}></a>
-    );
-}
--- src/utils/validateEmail.js
+++
@@ -1,7 +0,0 @@
-// https://stackoverflow.com/a/9204568/4674834
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-export default validateEmail;
